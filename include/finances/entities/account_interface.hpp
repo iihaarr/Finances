347df03766cc242ed79803e1045ed5ce7aaed3d2
Антонов Игорь Игоревич <iantonov@srv.rbt>
2024-01-30 12:29:30 +0300

    added category interface

--- include/finances/entities/account_interface.hpp
+++ include/finances/entities/account_interface.hpp
@@ -1,6 +1,8 @@
 #ifndef __ENTITIES_ACCOUNT_INTERFACE_HPP__
 #define __ENTITIES_ACCOUNT_INTERFACE_HPP__
 
+#include <string>
+
 #include "finances/utils/types.hpp"
 
 namespace finances::entities
@@ -9,6 +11,7 @@ namespace finances::entities
    {
       virtual finances::utils::types::ID GetID() const noexcept = 0;
       virtual std::string GetName() const noexcept = 0;
+      virtual ~IAccount() = default;
    };
 }
 
--- include/finances/entities/category_interface.hpp
+++ include/finances/entities/category_interface.hpp
@@ -1,9 +1,18 @@
 #ifndef __ENTITIES_CATEGORY_INTERFACE_HPP__
 #define __ENTITIES_CATEGORY_INTERFACE_HPP__
 
+#include <string>
+
+#include "finances/utils/types.hpp"
+
 namespace finances::entities
 {
-
+   struct ICategory
+   {
+      virtual finances::utils::types::ID GetID() const noexcept = 0;
+      virtual std::string GetName() const noexcept = 0;
+      virtual ~ICategory() = default;
+   };
 }
 
 #endif
